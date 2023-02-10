85d37881f6ab217fd5eac8c90a837c68d1224b0f
Andreas Heissenberger <andreas@heissenberger.at>
2023-02-10 10:50:00 +0100

    fix: replace string escape

--- src/obj2phparray.ts
+++ src/obj2phparray.ts
@@ -1,26 +1,20 @@
-export default function obj2phpArray(obj: object): string | object {
+export default function obj2phpArray(obj: any): string | object {
     if (typeof obj === 'string') {
-        return stringEscape2(obj)
-    } else if (obj === null) {
+        return stringEscape(obj)
+    } else if (obj === null || (typeof obj === 'undefined')) {
         return "NULL"
     } else if (typeof obj === 'boolean') {
         return obj ? 'true' : 'false'
     } else if (typeof obj === 'number') {
         return Number(obj).toString()
     } else if (Array.isArray(obj)) {
-        return `[${obj.map(c => obj2phpArray(c)).join(',')}]`
+        return `[${Array.from(obj).map(c => obj2phpArray(c)).join(',')}]`
     } else if (typeof obj === 'object') {
-        return `[${Object.entries(obj).map(([key, value]) => (`"${stringEscape(key)}"=>${obj2phpArray(value)}`))}]`
+        return `[${Object.entries(obj).map(([key, value]) => (`${stringEscape(key)}=>${obj2phpArray(value)}`))}]`
     }
-    throw new TypeError(`${typeof obj} is not suuported!`)
+    throw new TypeError(`${typeof obj} is not suported!`)
 }
 
 function stringEscape(raw: string) {
-    return raw.replaceAll('"', '\"')
-        .replaceAll("\n", '\\n')
-        .replaceAll("\r", '\\r')
-}
-
-function stringEscape2(raw: string) {
     return JSON.stringify(raw)
 }
